0a13fb62ade883850f7f3874901339d230d62d59
Nadir Yuceer <nadir.yuceer@gmail.com>
2024-12-01 13:37:13 +0300

    Add gui + game mechanics

--- main.gd
+++ main.gd
@@ -3,12 +3,30 @@ extends Node
 @export var rock_scene : PackedScene
 
 var screensize = Vector2.ZERO
+var level = 0
+var score = 0
+var playing = false
+
+func new_game():
+	# remove any old rocks from previous game
+	get_tree().call_group("rocks", "queue_free")
+	level = 0
+	score = 0
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready!")
+	$Player.reset()
+	await $HUD/Timer.timeout
+	playing = true
+
+func new_level():
+	level += 1
+	$HUD.show_message("Wave %s" % level)
+	for i in level:
+		spawn_rock(3)
 
 func _ready():
 	screensize = get_viewport().get_visible_rect().size
 	$Player.position = Vector2(screensize.x / 2, screensize.y / 2)
-	for i in 3:
-		spawn_rock(3)
 
 
 func spawn_rock(size, pos=null, vel=null):
@@ -32,3 +50,13 @@ func _on_rock_exploded(size, radius, pos, vel):
 		var newvel = dir * vel.length() * 1.1
 		spawn_rock(size - 1, newpos, newvel)
 	
+
+func _process(_delta):
+	if not playing:
+		return
+	if get_tree().get_nodes_in_group("rocks").size() == 0:
+		new_level()
+		
+func game_over():
+	playing = false
+	$HUD.game_over()
--- player/player.gd
+++ player/player.gd
@@ -15,6 +15,20 @@ var rotation_dir = 0
 
 var can_shoot = true
 
+signal lives_changed
+signal dead
+
+var reset_pos = false
+var lives = 0: set = set_lives
+
+func set_lives(value):
+	lives = value
+	lives_changed.emit(lives)
+	if lives <= 0:
+		change_state(DEAD)
+	else:
+		change_state(INVULNERABLE)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 		change_state(ALIVE)
@@ -25,14 +39,22 @@ func _ready():
 func change_state(new_state):
 	match new_state:
 		INIT:
-			$CollisionPolygon2D.set_deferred("disabled", true)
+			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.modulate.a = 0.5
 		ALIVE:
-			$CollisionPolygon2D.set_deferred("disabled", false)
+			$CollisionShape2D.set_deferred("disabled", false)
+			$Sprite2D.modulate.a = 1.0
 		INVULNERABLE:
-			$CollisionPolygon2D.set_deferred("disabled", true)
+			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.modulate.a = 0.5
+			$InvulnerabilityTimer.start()
 		DEAD:
-			$CollisionPolygon2D.set_deferred("disabled", true)
+			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.hide()
+			linear_velocity = Vector2.ZERO
+			dead.emit()
 	state = new_state
+
 
 func shoot():
 	if state == INVULNERABLE:
@@ -68,7 +90,35 @@ func _integrate_forces(physics_state):
 	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
 	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
 	physics_state.transform = xform
+	if reset_pos:
+		physics_state.transform.origin = screensize / 2
+		reset_pos = false
 
 
 func _on_gun_cooldown_timeout() -> void:
 	can_shoot = true
+
+func reset():
+	reset_pos = true
+	$Sprite2D.show()
+	lives = 3
+	change_state(ALIVE)
+
+
+func _on_invulnerability_timer_timeout() -> void:
+	change_state(ALIVE)
+
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("rocks"):
+		if state != INVULNERABLE:
+			body.explode()
+			lives -= 1
+			explode()
+
+
+func explode():
+	$Explosion.show()
+	$Explosion/AnimationPlayer.play("explosion")
+	await $Explosion/AnimationPlayer.animation_finished
+	$Explosion.hide()
